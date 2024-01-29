381f25e21cfc9e278a22255ed71e3e367480ba36
sugan0tech <sugankpms@gmail.com>
2024-01-29 12:08:27 +0530

    zip file extraction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import boto3
+import zipfile
+from bs4 import BeautifulSoup
 import os
 import json
 import dotenv
@@ -12,24 +14,38 @@ def download_file_from_s3(bucket_name, object_key, local_file_path):
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
     s3.download_file(bucket_name, object_key, local_file_path)
 
+def extract_content(local_zip_path:str):
+    with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
+        file_list = zip_ref.namelist()
+    
+        for file_name in file_list:
+            if file_name.endswith('.html'):
+                html_content = zip_ref.read(file_name)
+            
+                soup = BeautifulSoup(html_content, 'html.parser')
+            
+                elements = soup.find_all(class_='covered_percent')
+                    
+                return elements[0].text
+
 if __name__ == "__main__":
     bucket_name = 'testvini'
     object_key = 'test/hi.txt'
-    local_file_path = "/home/sugan/Documents/test/hi.txt"
+    local_file_path = "/home/sugan/Documents/test/coverage-2024-01-17.zip"
+    coverage = extract_content(local_file_path)
+
 
     download_file_from_s3(bucket_name, object_key, local_file_path)
-    # process_zip_file(local_file_path)
+    process_zip_file(local_file_path)
 
     client = WebClient(os.environ["SLACK_BOT_TOKEN"])
 
-    file_contents = ""
-
-    with open(local_file_path, 'rb') as file:
+    with open( "/home/sugan/Documents/test/coverage-2024-01-17.zip", 'rb') as file:
         file_contents = file.read()
 
-    new_file = client.files_upload_v2(
-    channels='C03GEKPH135',
-    title="Coverage",
-    filename="test.txt",
-    file=file_contents
-)
+        new_file = client.files_upload_v2(
+            channels='C03GEKPH135',
+            title=f"Coverage : {coverage}",
+            filename="coverage.zip",
+            file=file_contents
+        )
